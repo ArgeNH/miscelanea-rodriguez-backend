7e46d2a37e83d574f88c6e543b1147a83300f9cc
Arge Niño <arge.nino@uptc.edu.co>
2022-03-14 10:50:02 -0500

    fix route for payments

--- controller/payments.js
+++ controller/payments.js
@@ -1,86 +1,89 @@
 const axios = require('axios');
+const { response } = require('express');
 const { token } = require('morgan');
 
- 
- const createOrder =async (req,res ) =>{
-    const { products, value } = req.body;
-    console.log(products);
-    console.log(value);
-     try {
-        const order = {
-            intent: "CAPTURE",
-            //productos
-            purchase_units:[
-                {
-                    amount:{
-                        currency_code:"USD",
-                        value: value,
-                    },
-                    items: products,
-   
-                },
-               ],
-               application_context: {
-                   brand_name:"NaturSalud.life",
-                   landing_page: "LOGIN",
-                   user_action:"PAY_NOW",
-                   return_url:"http://localhost:5000/api/payments/getOrder",
-                   cancel_url:"http://localhost:5000/api/payments/cancel",
-               },
-        };
-        const params = new URLSearchParams();
-        params.append("grant_type","client_credentials");
-        const {
-            data:{access_token},
-           } = await axios.post(`https://api-m.sandbox.paypal.com/v1/oauth2/token`,
-           params,
-           {
-            headers:{
-                'Content-Type':'application/x-www-form-urlencoded',
+const createOrder = async (req, res = response) => {
+   const { value } = req.body;
+
+   const total = value * 0.00026;
+   const totalnotDecimal = Math.ceil(total);
+
+   try {
+      const order = {
+         intent: "CAPTURE",
+         //productos
+         purchase_units: [
+            {
+               amount: {
+                  currency_code: "USD",
+                  value: totalnotDecimal,
+               }
             },
-            auth:{
+         ],
+         application_context: {
+            brand_name: "miscelanearodriguez.life",
+            landing_page: "LOGIN",
+            user_action: "PAY_NOW",
+            return_url: "http://localhost:5000/api/payments/getOrder",
+            cancel_url: "http://localhost:5000/api/payments/cancel",
+         },
+      };
+      const params = new URLSearchParams();
+      params.append("grant_type", "client_credentials");
+      const {
+         data: { access_token },
+      } = await axios.post(`https://api-m.sandbox.paypal.com/v1/oauth2/token`,
+         params,
+         {
+            headers: {
+               'Content-Type': 'application/json',
+            },
+            auth: {
                username: process.env.PAYPAL_API_CLIENT,
                password: process.env.PAYPAL_API_SECRET,
             }
-        });
-       const respom = await axios.post(`${process.env.PAYPAL_API}/v2/checkout/orders`,order,{
-           headers:{
-               Authorization: `Bearer ${access_token}`
-           }
-           /* auth:{
-                username: process.env.PAYPAL_API_CLIENT,
-                password: process.env.PAYPAL_API_SECRET,
-            },*/
-             
-        });
-       
-        res.json(respom.data); 
+         });
+      const respom = await axios.post(`${process.env.PAYPAL_API}/v2/checkout/orders`, order, {
+         headers: {
+            Authorization: `Bearer ${access_token}`
+         }
+      });
+
+      return res.status(200).json({
+         success: true,
+         response: respom.data
+      });
       // console.log(respom.data);
-     } catch (error) {
-         return res.status(500).send('Something goes wrong');
-     }
-    
+   } catch (error) {
+      return res.status(500).json({
+         success: false,
+         error
+      });
+   }
+
 };
 
- const capOrder =async (req,res ) =>{
-     const {token,PayerID} =req.query
-    const reponse = await axios.post(`${process.env.PAYPAL_API}/v2/checkout/orders/${token}/capture`,{},{
-        auth:{
-            username: process.env.PAYPAL_API_CLIENT,
-            password: process.env.PAYPAL_API_SECRET,
-        },
-    })
-    // console.log(token,PayerID);
-    console.log(reponse.data);
-    return res.redirect('');//link o direccion de la pagian ala cual lo va redireccionar
-}
- const cancel =(req,res ) =>{
-     res.redirect('')//pagina inicial 
-    res.send('cancelar orden')
+const capOrder = async (req, res) => {
+   const { token } = req.query
+   const response = await axios.post(`${process.env.PAYPAL_API}/v2/checkout/orders/${token}/capture`, {}, {
+      auth: {
+         username: process.env.PAYPAL_API_CLIENT,
+         password: process.env.PAYPAL_API_SECRET,
+      },
+   })
+   return res.status(200).json({
+      success: true,
+      message: 'Se hizo la orden',
+      response: response.data
+   })
 }
 
-module.exports ={
-    capOrder,
-    createOrder,
-    cancel
+const cancel = (req, res) => {
+   res.redirect('https://www.miscelanearodriguez.life');
+}
+
+module.exports = {
+   capOrder,
+   createOrder,
+   cancel
 }
--- routes/payment.js
+++ routes/payment.js
@@ -1,14 +1,12 @@
 const { Router } = require('express');
-const {createOrder,capOrder,cancel} = require('../controller/payments');
+const { createOrder, capOrder, cancel } = require('../controller/payments');
 
 const router = Router();
 
-router.post('/orderBuy',createOrder);
+router.post('/orderBuy', createOrder);
 
-router.get('/getOrder',capOrder);
+router.get('/getOrder', capOrder);
 
-router.get('/cancel',cancel);
- 
- 
- 
+router.get('/cancel', cancel);
+
 module.exports = router;
